6e78e28075436c9463c2cadb7e8ff9a0538a67bb
Rohith441438 <rohithvinay889@gmail.com>
2023-07-02 23:39:47 +0530

    02-07-2023  - Extension methods example added

---
+++ Project/Project/ExtensionMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class ExtensionMethod
+    {
+        public static void CheckExtensionMethod()
+        {
+            ClassToBeExtended classToBeExtended = new ClassToBeExtended();
+            classToBeExtended.SayIamExtension();
+        }
+    }
+
+    static class ExtensionClass
+    {
+        public static void SayIamExtension(this ClassToBeExtended ex)
+        {
+            Console.WriteLine("Yes I am extension Method");
+        }
+    }
+    class ClassToBeExtended
+    {
+        public void SayHai()
+        {
+            Console.WriteLine("Say Hai");
+        }
+    }
+}
---
+++ Project/Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using Project;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //This class explains how an Extension methods works in c#
+            ExtensionMethod.CheckExtensionMethod();
+        }
+    }
+}
