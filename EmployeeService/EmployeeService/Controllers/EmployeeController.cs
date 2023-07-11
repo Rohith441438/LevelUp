328cf43a41ee0835cb5821a74f476bda361e064c
Rohith441438 <rohithvinay889@gmail.com>
2023-07-11 23:45:28 +0530

    Employee Service Http Verbs - Post method is added by mentioning wrong and correct ways of using - Put method is added by mentioning wrong and correct ways of using - Delete method is added by mentioning wrong and correct ways of using

--- EmployeeService/EmployeeService/Controllers/EmployeeController.cs
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -35,5 +35,122 @@ namespace EmployeeService.Controllers
                 }
             }
         }
+
+        //public void Post(Employee employee)
+        //{
+        //    using(EmployeeDBContext employeeDBContext = new EmployeeDBContext())
+        //    {
+        //        employeeDBContext.Employees.Add(employee);
+        //        employeeDBContext.SaveChanges();
+        //    }
+        //}
+
+        ////Here the above method will return a void cause of this we will get No Content response status which is not a good practice, to Solve this issue follow the below way of Posting
+        ///
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using(EmployeeDBContext employeeDBContext = new EmployeeDBContext())
+                {
+                    employeeDBContext.Employees.Add(employee);
+                    employeeDBContext.SaveChanges();
+
+                    //Always return a responsemessage after successfully post to DB, with statusCode 201
+                    var responseMessage = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    //Try to add location of the response in the response message as like below
+                    responseMessage.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+
+                    return responseMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                var responseMessage = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+                
+                return responseMessage;
+            }
+        }
+
+        //Http Put verbs
+        //public void Put(int id, [FromBody] Employee employee)
+        //{
+        //    using(EmployeeDBContext dbContext = new EmployeeDBContext())
+        //    {
+        //        var entity = dbContext.Employees.FirstOrDefault(x => x.ID == id);
+
+        //        entity.FirstName = employee.FirstName;
+        //        entity.LastName = employee.LastName;
+        //        entity.Salary = employee.Salary;
+        //        entity.Gender = employee.Gender;
+
+        //        dbContext.SaveChanges();
+        //    }
+        //}
+        ////The above method will work but it response back with 204 status code which is not a good practice, see the below method which will return a HttpResponseMessage
+
+        public HttpResponseMessage Put(int id, [FromBody]Employee employee)
+        {
+            try
+            {
+                using(EmployeeDBContext employeeDBContext = new EmployeeDBContext())
+                {
+                    var entity = employeeDBContext.Employees.FirstOrDefault(x => x.ID==id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " Not Found");
+                    }
+                    entity.FirstName = employee.FirstName;
+                    entity.LastName = employee.LastName;
+                    entity.Salary = employee.Salary;
+                    entity.Gender = employee.Gender;
+
+                    employeeDBContext.SaveChanges();
+                    
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        //Http Delete verbs
+        //public void Delete(int id)
+        //{
+        //    using (EmployeeDBContext dbContext = new EmployeeDBContext())
+        //    {
+        //        dbContext.Employees.Remove(dbContext.Employees.FirstOrDefault(x => x.ID == id));
+
+        //        dbContext.SaveChanges();
+        //    }
+        //}
+        ////The above method will work but it response back with 204 status code which is not a good practice, see the below method which will return a HttpResponseMessage
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBContext employeeDBContext = new EmployeeDBContext())
+                {
+                    var entity = employeeDBContext.Employees.FirstOrDefault(x => x.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " Not Found");
+                    }
+                    employeeDBContext.Employees.Remove(entity);
+
+                    employeeDBContext.SaveChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
     }
 }
