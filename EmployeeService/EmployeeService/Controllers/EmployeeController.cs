2c8e30d48b3539509268cec03aafade598913cb7
Rohith441438 <rohithvinay889@gmail.com>
2023-07-11 19:36:55 +0530

    Employee Controller - Returning HttpResponeMessage instead of Normal Response

---
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                var employees = dbContext.Employees.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, employees);
+            }
+        }
+
+        public HttpResponseMessage GetById(int id)
+        {
+            using(EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                var employee = dbContext.Employees.FirstOrDefault(x => x.ID == id);
+                
+                if(employee != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, employee);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id "+id.ToString() + " is not found");
+                }
+            }
+        }
+    }
+}
