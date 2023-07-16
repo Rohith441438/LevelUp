208de792612eb6e879ba5f824abb62cddde8901b
Rohith441438 <rohithvinay889@gmail.com>
2023-07-16 16:27:33 +0530

    CORS - Cross Origin Resource Sharing will help web browsers or respective services to use APIs from different Origins

--- EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace EmployeeService
 {
@@ -19,6 +20,14 @@ namespace EmployeeService
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //CORS - Cross Origin Resource Sharing
+            //If we are accessing an API which is from different origin then browsers will not allow as they are not from same origin, in this case we should Enablecors in that API, lets see how we can Enable
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            //Here the three parameters are Origins, Headers and Methods, we provide * means we are allowing all origins, headers and methods
+            //we can provide comma separated Origins, Headers and methods in the parameters
+            config.EnableCors(cors);
         }
     }
 }
